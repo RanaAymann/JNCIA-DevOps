c7e55b1fde15da8aaf887c6801d62180acfbf6c3
Rana Ayman <33266962+ranaelemam@users.noreply.github.com>
2020-08-15 19:40:26 +0300

    ansible edit:

---
+++ Ansible/setup.sh
@@ -0,0 +1,9 @@
+# 2 Junos packages PyEz and jxmlease 
+
+pip install ansible
+pip install junos-eznc
+pip install jxmlease
+
+# juniper.junos Ansible Galaxy role it's an app store for ansible
+ansible-galaxy install Juniper.junos
+
