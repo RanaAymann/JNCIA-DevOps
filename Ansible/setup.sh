8a619457a83a205e8eb9155fa7c2e572b425821e
Rana Ayman <33266962+ranaelemam@users.noreply.github.com>
2020-08-16 09:11:36 +0300

    inventory file

--- Ansible/setup.sh
+++ Ansible/setup.sh
@@ -7,3 +7,15 @@ pip install jxmlease
 # juniper.junos Ansible Galaxy role it's an app store for ansible
 ansible-galaxy install Juniper.junos
 
+# go to - cat /etc/ansible/hosts
+# first devices managed by Ansible
+vMX-1
+vMX-2
+
+[vmx-devices]
+vMX-1
+vMX-2
+
+[vmx-devices:vars]
+ntp_server = 172.25.11.254
+
