5baf5a036d10d05b43266d5103d71eeac48fa89b
Rana Ayman <33266962+ranaelemam@users.noreply.github.com>
2020-08-20 20:50:42 +0300

    PyEZ 2

---
+++ PyEZ2.py
@@ -0,0 +1,16 @@
+# get route information for inet 0
+
+# !/usr/bin/python
+from jnpr.junos import Device
+
+if __name__ == '__main__':
+
+    dev = Device(host="10.1.1.1", user="lab", password="lab123")
+    dev.open()
+    route_lxml_element = dev.rpc.get_route_information(table="inet.0")
+    list_of_routes = route_lxml_element.findall('.//rt')
+    for route in list_of_routes:
+        print("Route: {} Protocol: {} ".format(route.findtext('rt-destination').strip(),
+                                               route.findtext('rt-entry/protocol-name').strip()))
+
+    dev.close()
